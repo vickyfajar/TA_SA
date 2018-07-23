e1e8b2978707532b79d28cf91c0dc240dc8842fa
DevidGibran <39785070+DevidGibran@users.noreply.github.com>
2018-07-24 00:06:10 +0700

    Update DepthFirst.java

--- DepthFirst.java
+++ DepthFirst.java
@@ -24,5 +24,22 @@ public static boolean searchPath(int[][] maze, int x, int y, List<Integer> path)
                 path.add(y);
                 return true;
             }
+           dx = 0;
+            dy = -1;
+            if (searchPath(maze, x + dx, y + dy, path)) {
+                path.add(x);
+                path.add(y);
+                return true;
+            }
+            dx = 0;
+            dy = 1;
+            if (searchPath(maze, x + dx, y + dy, path)) {
+                path.add(x);
+                path.add(y);
+                return true;
+            }
+        }
+        return false;
+    }
    
 }
